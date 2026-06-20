1cfaf36f175ca74dcb3d7576cd56777c42d717b3
LAPTOP-TNLJHDMS\olive <oliveroldham13@Gmail.com>
2026-06-20 10:14:04 -0400

    add public decorator, and enable auth across entire project

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -3,19 +3,18 @@ import { AppService } from './app.service';
 import { AuthGuard } from '@nestjs/passport';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AuthService } from './auth/auth.service';
+import { Public } from './auth/auth.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService, private AuthService: AuthService) {}
 
-
-  @UseGuards(LocalAuthGuard)
+  @Public()
   @Post('/auth/login')
   async login(@Request() req){
     return this.AuthService.login(req.user);
   }
 
-  @UseGuards(LocalAuthGuard)
   @Post('auth/logout')
   async logout(@Request() req) {
     return req.logout();
--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -5,6 +5,8 @@ import { UsersModule } from '../users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt.strategy';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './jwt-auth.guard';
 
 @Module({
   imports: [
@@ -15,7 +17,7 @@ import { JwtStrategy } from './jwt.strategy';
       signOptions: { expiresIn: '60s' },
     }),
   ],
-  providers: [AuthService, LocalStrategy, JwtStrategy],
+  providers: [AuthService, LocalStrategy, JwtStrategy, {provide: APP_GUARD, useClass: JwtAuthGuard}],
   exports: [AuthService],
 })
 
