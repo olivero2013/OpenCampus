cb36ca95b45761f708570433129a4f7ed0fb1e3c
LAPTOP-TNLJHDMS\olive <oliveroldham13@Gmail.com>
2026-06-19 20:19:40 -0400

    add swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,9 +3,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { PrismaExceptionFilter } from './prisma/prisma.filter';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('OpenCampus SIS API')
+    .setDescription('Student Information System API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, document);
+
   app.useGlobalPipes(new ValidationPipe({transform: true}));
   app.useGlobalFilters(new PrismaExceptionFilter);
   await app.listen(process.env.PORT ?? 3000);
